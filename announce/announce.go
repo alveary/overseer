e129e471aa0a0fac6b4344926b0af0acb1dbccff
Falk Hoppe <falkhoppe81@gmail.com>
2015-02-27 21:35:45 +0100

    added overseer client

---
+++ announce/announce.go
@@ -0,0 +1,29 @@
+package attach
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+)
+
+// ServiceEntry ...
+type ServiceEntry struct {
+	ServiceName   string
+	ServiceRoot   string
+	AliveResource string
+}
+
+// Service provides a method to attach a new Service to the overseer stack
+func Service(entry ServiceEntry) {
+	json, _ := json.Marshal(entry)
+
+	overseerRoot := os.Getenv("OVERSEER_ROOT")
+
+	if overseerRoot == "" {
+		fmt.Println("OVERSEER_ROOT is not set:")
+	}
+
+	http.Post(overseerRoot, "application/json", bytes.NewBuffer(json))
+}
