6a0108285e3f4338182fc0c5b10a9d7d636d7805
Falk Hoppe <falkhoppe81@gmail.com>
2015-02-27 22:48:29 +0100

    updated announce func to re register after a while

--- watchdog/watchdog.go
+++ watchdog/watchdog.go
@@ -15,38 +15,42 @@ func Watch(service *registry.Service) {
 		available := true
 
 		for available {
-			checkchan := make(chan bool)
-			errorchan := make(chan error)
-			defer func() {
-				close(checkchan)
-				close(errorchan)
-			}()
 
 			go func() {
-				resp, err := http.Head(service.Alive)
-				if err != nil {
-					errorchan <- err
-					return
-				}
-				if resp.StatusCode > 299 {
-					fmt.Println(fmt.Sprintf("Watchdog Lookup: (%s) Status: %s", service.Alive, resp.StatusCode))
-					errorchan <- fmt.Errorf("Request Error: %s", resp)
-					return
+				checkchan := make(chan bool)
+				errorchan := make(chan error)
+				defer func() {
+					close(checkchan)
+					close(errorchan)
+				}()
+
+				go func() {
+					resp, err := http.Head(service.Alive)
+					if err != nil {
+						errorchan <- err
+						return
+					}
+					if resp.StatusCode > 299 {
+						fmt.Println(fmt.Sprintf("Watchdog Lookup: (%s) Status: %s", service.Alive, resp.StatusCode))
+						errorchan <- fmt.Errorf("Request Error: %s", resp.Status)
+						return
+					}
+
+					checkchan <- true
+				}()
+
+				select {
+				case <-checkchan:
+					// JUST GO ON
+				case <-errorchan:
+					service.AddFailure()
+				case <-time.After(time.Second * 3):
+					service.AddFailure()
 				}
 
-				checkchan <- true
+				time.Sleep(1 * time.Minute)
 			}()
 
-			select {
-			case <-checkchan:
-				// JUST GO ON
-			case <-errorchan:
-				service.AddFailure()
-			case <-time.After(time.Second * 3):
-				service.AddFailure()
-			}
-
-			time.Sleep(1 * time.Minute)
 		}
 
 	}()
