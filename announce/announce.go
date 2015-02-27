745ad3b71168f650d191a3e1e4321864bf16e441
Falk Hoppe <falkhoppe81@gmail.com>
2015-02-27 22:21:52 +0100

    fixed announcement client

--- announce/announce.go
+++ announce/announce.go
@@ -8,21 +8,23 @@ import (
 	"os"
 )
 
-// ServiceEntry ...
-type ServiceEntry struct {
-	ServiceName   string
-	ServiceRoot   string
-	AliveResource string
-}
-
-// Service provides a method to attach a new Service to the overseer stack
-func Service(entry ServiceEntry) {
-	json, _ := json.Marshal(entry)
+// NewService provides a method to attach a new Service to the overseer stack
+func NewService(serviceName string, serviceRoot string, aliveResource string) {
+	json, _ := json.Marshal(struct {
+		serviceName   string
+		serviceRoot   string
+		aliveResource string
+	}{
+		serviceName,
+		serviceRoot,
+		aliveResource,
+	})
 
 	overseerRoot := os.Getenv("OVERSEER_ROOT")
 
 	if overseerRoot == "" {
 		fmt.Println("OVERSEER_ROOT is not set:")
+		return
 	}
 
 	http.Post(overseerRoot, "application/json", bytes.NewBuffer(json))
