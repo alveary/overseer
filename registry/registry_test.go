3aa3fe652ff982e09cd0a96b386c19a93e77ece1
Falk Hoppe <falkhoppe81@gmail.com>
2015-03-11 10:31:06 +0100

    formatting

--- registry/registry_test.go
+++ registry/registry_test.go
@@ -14,6 +14,7 @@ type RegistryTestSuite struct {
 
 // func (suite *RegistryTestSuite) SetupTest() {
 // }
+
 func (suite *RegistryTestSuite) TestServiceRegistration() {
 	registry := Registry{make(map[string]interface{})}
 	newService := service.Service{"service-name", "root-url", "alive-url", 0}
