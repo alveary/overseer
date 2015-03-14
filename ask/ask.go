2428bb891b2c9d190d08c1b0d9f066463ac2ffb1
Falk Hoppe <falkhoppe81@gmail.com>
2015-03-14 18:24:08 +0100

    added api access library for overseer using services

--- ask/ask.go
+++ ask/ask.go
@@ -1,5 +1,46 @@
 package ask
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"os"
+	"time"
 
+	"github.com/alveary/overseer/service"
+)
+
+// Ask lets you retrieve the service URI information
+func Ask(serviceName string) (retrieved service.Service, err error) {
+	overseerRoot := os.Getenv("OVERSEER_ROOT")
+	responsechan := make(chan *http.Response)
+	errorchan := make(chan error)
+
+	go func() {
+		resp, err := http.Get(overseerRoot + "/" + serviceName)
+		if err != nil {
+			errorchan <- err
+			return
+		}
+		if resp.StatusCode > 299 {
+			errorchan <- fmt.Errorf("Request Error: %s", resp.Status)
+			return
+		}
+
+		responsechan <- resp
+	}()
+
+	select {
+	case resp := <-responsechan:
+		defer resp.Body.Close()
+
+		dec := json.NewDecoder(resp.Body)
+		dec.Decode(&retrieved)
+
+		return retrieved, nil
+	case err = <-errorchan:
+		return retrieved, err
+	case <-time.After(time.Second * 3):
+		return retrieved, fmt.Errorf("Timeout of service registry call")
+	}
 }
